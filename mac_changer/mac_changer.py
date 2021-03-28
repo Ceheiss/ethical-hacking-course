57dad8ca96e3db13e7ed4a5a07cfb20845dca5de
Ceheiss <cristobal.heiss.j@gmail.com>
2021-03-28 17:44:35 +0200

    add simple variables

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,8 +2,11 @@
 
 import subprocess
 
-# second argument is the interface name (should be variable)
-# mac number choice should also be variable
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+interface = "eth0"
+new_mac = "00:11:22:33:44:55"
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
